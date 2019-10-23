e68df683d0e75a5e55b6fecbbbc02589ceea02ba
matsumoto <matsumoto.k017a1377@gmail.com>
2019-10-23 15:13:11 +0900

    文字列系のユーティリティ追加

--- Raytrace/Framework/Utility/StringUtil.h
+++ Raytrace/Framework/Utility/StringUtil.h
@@ -8,9 +8,30 @@ namespace Framework::Utility {
     * @brief std::string‚©‚çstd::wstring‚É•ÏŠ·‚·‚é
     */
     inline std::wstring toWString(const std::string& str) {
-        int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-        std::wstring wstrTo(size_needed, 0);
-        MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-        return wstrTo;
+        const int len = ::MultiByteToWideChar(932, 0, str.c_str(), -1, nullptr, 0);
+        std::wstring res(len * 2 + 2, L'\0');
+        if (!::MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, &res[0], len)) {
+            throw std::runtime_error("function toWString ERROR");
+        }
+
+        const std::size_t real_len = std::wcslen(res.c_str());
+        res.resize(real_len);
+        res.shrink_to_fit();
+        return res;
+    }
+
+    /**
+    * @brief std::wstring‚©‚çstd::string‚É•ÏŠ·‚·‚é
+    */
+    inline std::string toString(const std::wstring& str) {
+        const int len = ::WideCharToMultiByte(932, 0, str.c_str(), -1, nullptr, 0, nullptr, nullptr);
+        std::string res(len * 2, '\0');
+        if (!::WideCharToMultiByte(CP_ACP, 0, str.c_str(), -1, &res[0], len, nullptr, nullptr)) {
+            throw std::runtime_error("function toString ERROR");
+        }
+        const std::size_t real_len = std::strlen(res.c_str());
+        res.resize(real_len);
+        res.shrink_to_fit();
+        return res;
     }
 } //Framework::Utility 
